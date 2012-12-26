a621165d644471e59bb2793f5abdf9ac296661ac
David Hoyt <dhoyt@hoytsoft.org>
2012-12-26 01:53:11 -0800

    Added ObjectGraph.

---
+++ src/main/java/jcommon/deps/ObjectGraph.java
@@ -0,0 +1,87 @@
+/*
+  Copyright (C) 2012-2013 the original author or authors.
+
+  See the LICENSE.txt file distributed with this work for additional
+  information regarding copyright ownership.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+package jcommon.deps;
+
+/**
+ * Factory and implementation of a dependency graph that can topologically sort vertices that are {@link Object}s.
+ *
+ * @see jcommon.deps.DependencyGraph
+ */
+public class ObjectGraph extends DependencyGraph<ObjectVertex> {
+  protected ObjectGraph() {
+    super();
+  }
+
+  /**
+   * @see jcommon.deps.DependencyGraph#copy()
+   */
+  public ObjectGraph copyAsObjectGraph() {
+    return copyDependencyGraph(this);
+  }
+
+  /**
+   * @see jcommon.deps.DependencyGraph#build(jcommon.deps.IVertex[])
+   */
+  public static ObjectGraph buildFromObjects(Object...vertices) {
+    final ObjectGraph g = new ObjectGraph();
+    for(Object v : vertices)
+      g.addVertex(v);
+    return g;
+  }
+
+  /**
+   * @see jcommon.deps.DependencyGraph#create()
+   */
+  public static ObjectGraph createForObjects() {
+    return buildFromObjects();
+  }
+
+  /**
+   * @see jcommon.deps.DependencyGraph#addVertex(jcommon.deps.IVertex)
+   */
+  public ObjectGraph addVertex(Object vertex) {
+    addVertex(ObjectVertex.from(vertex));
+    return this;
+  }
+
+  /**
+   * @see jcommon.deps.DependencyGraph#removeVertex(jcommon.deps.IVertex)
+   */
+  public ObjectGraph removeVertex(Object vertex) {
+    removeVertex(ObjectVertex.from(vertex));
+    return this;
+  }
+
+  /**
+   * @see jcommon.deps.DependencyGraph#addEdge(jcommon.deps.IVertex, jcommon.deps.IVertex)
+   */
+  public ObjectGraph addEdge(ObjectVertex from, ObjectVertex to) {
+    addEdge(ObjectVertex.from(from), ObjectVertex.from(to));
+    return this;
+  }
+
+  /**
+   * @see jcommon.deps.DependencyGraph#removeEdge(jcommon.deps.IVertex, jcommon.deps.IVertex)
+   */
+  public ObjectGraph removeEdge(ObjectVertex from, ObjectVertex to) {
+    removeEdge(ObjectVertex.from(from), ObjectVertex.from(to));
+    return this;
+  }
+}
